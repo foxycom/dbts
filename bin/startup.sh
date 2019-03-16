0380aa4f335da1b1d1dc8f00f5ce0a96618e0dda
liukun <451564319@qq.com>
2019-03-16 16:45:10 +0800

    change the start up

--- bin/startup.sh
+++ bin/startup.sh
@@ -25,7 +25,47 @@ else
     JAVA=java
 fi
 
+pid=$$
+
+
+echo "================="
+echo "the job pid is ${pid}"
+echo "================="
+
+mkdir ${pid}
+
+cd ${pid}
+
+pidstat -p ${pid} -u  5 > cpu.log &
+pidstat -p ${pid} -r  5 > mem.log &
+pidstat -p ${pid} -w 5 > context.log &
+iostat -x 5 | grep "sdb"  > iotstat.log &
+cd ..
+
 
 exec "$JAVA" -Duser.timezone=GMT+8 -Dlogback.configurationFile=${BENCHMARK_HOME}/conf/logback.xml  -cp "$CLASSPATH" "$MAIN_CLASS" "$@"
+
+killpid=$(ps -u| grep "liurui" | grep "iostat" -m 1 | awk '{print $2}')
+
+echo "========iostat pid======="
+echo ${killpid}
+
+kill -9 ${killpid}
+kill -9 ${killpid}
+killpid=$(ps -u| grep "liurui" | grep "pidstat" -m 1 | awk '{print $2}')
+
+kill -9 ${killpid}
+
+killpid=$(ps -u| grep "liurui" | grep "pidstat" -m 1 | awk '{print $2}')
+
+kill -9 ${killpid}
+
+killpid=$(ps -u| grep "liurui" | grep "pidstat" -m 1 | awk '{print $2}')
+
+kill -9 ${killpid}
+
+echo "================="
+echo "the job pid is ${pid}"
+echo "================="
 
 exit $?
