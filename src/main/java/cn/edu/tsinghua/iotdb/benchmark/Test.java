fe7da9b89b866ad0788df7e068fd9829c5bf44cf
Tim <timvseffolod@gmail.com>
2019-08-27 17:56:23 +0200

    Temp fix

---
+++ src/main/java/cn/edu/tsinghua/iotdb/benchmark/Test.java
@@ -0,0 +1,10 @@
+package cn.edu.tsinghua.iotdb.benchmark;
+
+import cn.edu.tsinghua.iotdb.benchmark.sersyslog.CpuUsage;
+import cn.edu.tsinghua.iotdb.benchmark.sersyslog.MemUsage;
+
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
