7d2dae290a508a40af466388414f298e94f99d51
Vsevolod Tymofyeyev <timvseffolod@gmail.com>
2019-08-29 13:39:23 +0200

    fix routine

--- routine.sh
+++ routine.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+CONFIG_PATH="conf/config.properties"
 FILENAME="routine"
+COUNTER=0
 
+sed -i "s/BENCHMARK_WORK_MODE=.*/BENCHMARK_WORK_MODE=test_with_default_path/" "$DIR/$CONFIG_PATH"
 
 while IFS= read -r line
 do
     export IFS=" "
     for setting in $line; do
         if [ "$setting" == "TEST" ]; then
-            sh "cli-benchmark.sh"
+            printf "####################### TEST NUMBER %d #######################\n" $COUNTER
+            COUNTER=$((COUNTER+1))
+            sh "./cli-benchmark.sh"
         else
             IFS="="
             read -ra parts <<< "$setting"
 
             #printf "${parts[0]}\n"
-            sed -i '' "s/${parts[0]}=.*/${setting}/" ${DIR}'/conf/config.properties'
+            sed -i "s/${parts[0]}=.*/${setting}/" ${DIR}'/conf/config.properties'
         fi
     done
 done < "$DIR/$FILENAME"
